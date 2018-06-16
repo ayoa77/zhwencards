4ff96a0c3193ab50e6647721ae92d055e0d248e5
ayoa77 <ayodeleamadi@gmail.com>
2018-06-16 19:15:11 +0800

    pushing csjt fixes

--- app.js
+++ app.js
@@ -94,9 +94,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/flashcards/', express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/flashcards/index', indexRouter);
 app.use('/flashcards', decksRouter);
 
 // catch 404 and forward to error handler
